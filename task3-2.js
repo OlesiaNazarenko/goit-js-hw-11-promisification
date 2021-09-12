a87541d4241c8fab7fa07d4633b23856dc7bbf3b
OlesiaNazarenko <eightpa8@gmail.com>
2021-09-12 14:06:59 +0300

    task3

---
+++ task3-2.js
@@ -0,0 +1,27 @@
+/////////task 2///////////////
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+return Promise.resolve(allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user));
+};
+
+const logger = resolve => console.table(resolve);
+
+// /*
+//  * Сейчас работает так
+//  */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+// /*
+//  * Должно работать так
+//  */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
